6d7a918ce4fe268558c40a115f13ebf3ad3e5653
Duong <d@dttt.io>
2024-12-10 11:14:14 +0100

    Add inference

---
+++ scripts/inference.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+DS_PATH="dataset.json"
+MODEL_TYPE=""
+
+case $1 in
+# ======================================== mPLUGOwl3
+  1)
+    CONDA_ENV="owl3"
+    MODEL_NAME="mPLUGOwl3"
+    ;;
+
+esac
+
+
+source activate $CONDA_ENV
+
+OUTPUT_NAME=${MODEL_NAME}_${MODEL_TYPE}
+
+python -m benchmark.start \
+  --ds_path $DS_PATH \
+  --model_name $MODEL_NAME \
+  --model_type $MODEL_TYPE \
+  --output_name output_${OUTPUT_NAME}
