4dd85217893d68a0e46a6edaef04146b4de4ca7e
Duong Tran <d@dttt.io>
2025-01-29 23:04:06 +0100

    Added GPT-4o

--- models/GPT.py
+++ models/GPT.py
@@ -12,7 +12,7 @@ class GPT(BenchmarkModel):
     def load_model(self):
         # do not forget to set the environment variables for AZURE_OPENAI_API_KEY and AZURE_OPENAI_ENDPOINT
         self.client = AzureOpenAI(
-            api_version="2024-11-20"
+            api_version="2023-07-01-preview"
         )
 
     def run_vqa_task(self, image, row_data, choices=None, image_url=None):
