849fbf105f43cca129b8c506419bb64e5f53a985
Duong Tran <d@dttt.io>
2025-01-29 20:49:28 +0100

    Added GPT-4o

--- lsf_v100.sh
+++ lsf_v100.sh
@@ -1,4 +1,4 @@
-#BSUB -J Qwen[69]               # Specify the job name
+#BSUB -J GPT[69]               # Specify the job name
 #BSUB -W 168:00                # Specify the maximum runtime in "hours:minutes"
 #BSUB -o %x.%j.out            # Determine where the output will be written
 #BSUB -e %x.%j.err            # The same goes for the error file
