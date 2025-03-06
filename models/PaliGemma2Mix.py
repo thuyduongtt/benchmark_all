e779e69bc5ee9019a5d22b0441df2ac0d1818584
Duong Tran <d@dttt.io>
2025-03-07 00:27:51 +0100

    Finetune on PaliGemma2Mix

--- models/PaliGemma2Mix.py
+++ models/PaliGemma2Mix.py
@@ -20,11 +20,9 @@ class PaliGemma2Mix(BenchmarkModel):
         model = PaliGemmaForConditionalGeneration.from_pretrained(self.MODEL_PATH,
                                                                   token=self.access_token,
                                                                   torch_dtype=torch.bfloat16,
-                                                                  device_map="auto",
-                                                                  force_download=True).eval()
+                                                                  device_map="auto").eval()
         image_processor = PaliGemmaProcessor.from_pretrained(self.MODEL_PATH,
-                                                             token=self.access_token,
-                                                             force_download=True)
+                                                             token=self.access_token)
         self.model = model
         self.processor = image_processor
 
